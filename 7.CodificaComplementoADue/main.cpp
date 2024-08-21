2374a58bf62940c0b0e111a5b405a3dd3c0bd8b1
Andrii <148819392+andriikamanin@users.noreply.github.com>
2024-08-21 21:09:52 +0200

    добавил функции для декодировки

--- 7.CodificaComplementoADue/main.cpp
+++ 7.CodificaComplementoADue/main.cpp
@@ -6,6 +6,8 @@ using namespace std;
 
 void inputNumeroDecimale(int &);
 void codificaComplementoADue(int&);
+void inputNumeroBinario(bitset<8>&);
+void decodificaComplementoADue(bitset<8>&);
 int main()
 {
     int numero;
@@ -18,6 +20,10 @@ void inputNumeroDecimale(int &numero) {
     cout<<"Inserisci un numero decimale: ";
     cin>>numero;
 }
+void inputNumeroBinario(bitset<8>&numeroBinario){
+    cout<<"Inserisci un numero binario: ";
+    cin>>numeroBinario;
+}
 
 void codificaComplementoADue(int &numero){
     if(numero>0){
@@ -27,6 +33,15 @@ void codificaComplementoADue(int &numero){
         complemento = ~complemento;
         complemento = complemento.to_ulong() + 1;
         cout<<"Complemento a due: "<<complemento;
+    }
+}
+void decodificaComplementoADue(bitset<8>&numeroBinario){
+    if(numeroBinario[7] == 1){
+        bitset<8>numeroInverso = ~numeroBinario;
+        int numeroDecoficato = numeroInverso.to_ulong() + 1;
+        cout<<"Numero decimale decodificato: "<<-numeroDecoficato<<endl;
+    }else{
+        cout<<"Numero dopo conversione: "<<numeroBinario.to_ulong()<<endl;
     }
 }
 
