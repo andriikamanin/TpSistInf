bee8458ed3254842b2c4e9c1be4b4b0ba0ed4e7d
andriikamanin <andreikamanin2006@gmail.com>
2024-08-28 21:59:57 +0200

    добавление проекта номер 9

---
+++ 9.GeneratoreNumeriPrimi/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <bitset>
+#include <cmath>
+using namespace std;
+#define MAX_BITS 8
+void generatoreDiNumeriPrimi(bitset<MAX_BITS>&primo);
+void outputNumeriPrimi(bitset<MAX_BITS>&primo);
+
+
+int main() {
+    // a. Chiedere all’utente una sequenza binaria di 4 bit
+    bitset<MAX_BITS>primo;
+    generatoreDiNumeriPrimi(primo);
+    outputNumeriPrimi(primo);
+    return 0;
+}
+
+void generatoreDiNumeriPrimi(bitset<MAX_BITS>&primo){
+    primo.set();//impostiamo tutti i bit come 1 
+    primo[0] = primo[1] = 0;//0 e 1 non sono numeri primi
+    
+    for(int i=2; i<= sqrt(MAX_BITS); ++i){
+        if(primo[i]){
+            for(int j = i*i; j<MAX_BITS; j += i){
+                primo[j] = 0;
+            }
+        }
+    }
+}
+
+void outputNumeriPrimi(bitset<MAX_BITS>&primo){
+    for(int i=2; i<MAX_BITS;i++){
+        if(primo[i]){
+            cout<<i<<" ";
+        }
+        
+    }
+}
+
